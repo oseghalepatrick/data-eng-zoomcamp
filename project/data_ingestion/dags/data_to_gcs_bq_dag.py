bfce4ed8c31bc536e5cf39c70f852abe464d20ee
oseghalepatrick <oseghale_patrick@yahoo.com>
2022-03-19 00:23:42 +0000

    start data ingestion

---
+++ project/data_ingestion/dags/data_to_gcs_bq_dag.py
@@ -0,0 +1,20 @@
+import os
+import logging
+
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator, BigQueryInsertJobOperator
+from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
+
+from google.cloud import storage
+
+import pyarrow.csv as pv
+import pyarrow.parquet as pq
+
+PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
+BUCKET = os.environ.get("GCP_GCS_BUCKET")
+
+path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
