21d523b722f3c43d5d257e9d5a0a2980113fc8e4
oseghalepatrick <oseghale_patrick@yahoo.com>
2022-03-21 00:44:40 +0000

    update Dockerfile

--- project/data_ingestion/dags/data_to_gcs_bq_dag.py
+++ project/data_ingestion/dags/data_to_gcs_bq_dag.py
@@ -13,8 +13,27 @@ from google.cloud import storage
 import pyarrow.csv as pv
 import pyarrow.parquet as pq
 
+import pyspark
+from pyspark.sql import SparkSession, types
+
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 
-path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
-BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'all_movies_data')
+
+
+spark = SparkSession.builder \
+    .master("local[*]") \
+    .appName('test') \
+    .getOrCreate()
+
+def read_data(file_name, dest_file, schema=None):
+    url = f'https://datasets.imdbws.com/{file_name}.tsv.gz'
+    spark.sparkContext.addFile(url)
+    df = spark.read \
+            .option('header', 'true') \
+            .csv('file://'+pyspark.SparkFiles.get(file_name+'.tsv.gz'), sep='\t', schema=schema)
+    df.repartition(7)\
+        .write.parquet(dest_file, mode='overwrite')
+
